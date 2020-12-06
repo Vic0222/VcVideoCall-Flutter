039ff0654bcca4ef93d09f82cdcae4266cd1de78
Vic <v.g.albonian@gmail.com>
2020-12-06 20:20:29 +0800

    added url launche closes #28

--- lib/components/message_container.dart
+++ lib/components/message_container.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_parsed_text/flutter_parsed_text.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MessageContainer extends StatelessWidget {
   const MessageContainer({
@@ -27,23 +28,66 @@ class MessageContainer extends StatelessWidget {
               style: textStyle.copyWith(
                 decoration: TextDecoration.underline,
               ),
-              onTap: (url) {}),
+              onTap: (url) {
+                _launchEmail(url, context);
+              }),
           MatchText(
-            type: ParsedType.URL,
-            style: textStyle.copyWith(
-              decoration: TextDecoration.underline,
-            ),
-          ),
+              type: ParsedType.URL,
+              style: textStyle.copyWith(
+                decoration: TextDecoration.underline,
+              ),
+              onTap: (String url) {
+                _launchUrl(url, context);
+              }),
           MatchText(
               type: ParsedType.PHONE,
               style: textStyle.copyWith(
                 decoration: TextDecoration.underline,
               ),
               onTap: (url) {
-                print(url);
+                _launchTelephone(url, context);
               }),
         ],
       ),
     );
+  }
+
+  void _launchEmail(url, BuildContext context) {
+    Uri uri = Uri.parse(url);
+    if (!uri.hasScheme) {
+      uri = uri.replace(scheme: "mailto");
+    }
+
+    url = uri.toString();
+    _launch(url, context);
+  }
+
+  void _launchTelephone(url, BuildContext context) {
+    Uri uri = Uri.parse(url);
+    if (!uri.hasScheme) {
+      uri = uri.replace(scheme: "tel");
+    }
+
+    url = uri.toString();
+    _launch(url, context);
+  }
+
+  void _launchUrl(String url, BuildContext context) {
+    Uri uri = Uri.parse(url);
+    if (!uri.hasScheme) {
+      uri = uri.replace(scheme: "https");
+    }
+
+    url = uri.toString();
+    _launch(url, context);
+  }
+
+  Future<void> _launch(String url, BuildContext context) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      Scaffold.of(context)
+          .showSnackBar(SnackBar(content: Text("Can't launch url")));
+    }
   }
 }
